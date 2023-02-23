70a087eb0010ff3231211491e82ae1204d1f91ef
Omar <Omar@DESKTOP-CP8NGJ1>
2023-02-23 23:44:35 +0300

    Add project files.

---
+++ MersalAppStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MersalAppStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
